9ba3dd1ab1b562bf7476b13931501ad75f96052f
pinghoyk <pinghoyk009@gmail.com>
2025-11-09 02:11:46 +0500

    Новая структура user

--- internal/storage/models.go
+++ internal/storage/models.go
@@ -1,9 +1,12 @@
 // описание структур базы данных
 package models
+import "time"
 
 type User struct {
-    ID 			int64  	`db:"id" 		  json:"id"`
-    Name  		string 	`db:"name"		  json:"name"`
-    Currency 	string 	`db:"currency"  json:"currency"`
-    Quantity 	int 	`db:"quantity"  json:"quantity"`
+    ID 			int64  	    `db:"id" 		  json:"id"`
+    Email       string      `db:"email"       json:"email"`
+    Password  	string      `db:"password"	  json:"-"`
+    Name        string      `db:"name"        json:"name"`
+    CreatedAt   time.Time	`db:"created_at"  json:"created_at"`
+    UpdatedAt   time.Time   `db:"updated_at"  json:"updated_at"`
 }
