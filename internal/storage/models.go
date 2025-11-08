19f82dc63ecdeda89a1942b3cc91f9691943dc30
pinghoyk <pinghoyk009@gmail.com>
2025-11-09 01:30:32 +0500

    Создала стуктуру для бд

--- internal/storage/models.go
+++ internal/storage/models.go
@@ -1,1 +1,9 @@
 // описание структур базы данных
+package models
+
+type User struct {
+    ID 			int64  	`db:"id" 		  json:"id"`
+    Name  		string 	`db:"name"		  json:"name"`
+    Currency 	string 	`db:"currency"  json:"currency"`
+    Quantity 	int 	`db:"quantity"  json:"quantity"`
+}
