9f5e4c47a64bc0b9fb94f995158c92e87e00945d
pinghoyk <pinghoyk009@gmail.com>
2025-11-09 14:41:44 +0500

    Создаем либо открываем бд

--- internal/storage/sqlite.go
+++ internal/storage/sqlite.go
@@ -11,3 +11,9 @@ import (
 
 //go:embed schema.sql
 var schemaFS embed.FS
+
+func NewDB(path string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", path+"?_fk=1&_journal=WAL")
+	if err != nil {
+		return nil, fmt.Errorf("Не удалось открыть БД %q: %w", path, err)
+	}
